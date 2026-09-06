ee8d04183cece33b4a1dba502c4eb4e032f64547
lostincommits <wonderrhymes7@gmail.com>
2026-09-06 13:15:44 +0530

    Add weather checking function to Script.js

---
+++ Script.js
@@ -0,0 +1,7 @@
+const developer = "Definitely not a robot";
+
+function checkWeather() {
+    console.log("Checking the weather...");
+    console.log("Still checking...");
+    console.log("Maybe go outside and look? 😂");
+}
